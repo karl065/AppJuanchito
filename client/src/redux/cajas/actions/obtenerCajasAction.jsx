25d6605e220385bfedd0ee3381b94f6bd16ac4ee
Carlos Castellanos <karlos065@hotmail.com>
2025-12-07 23:12:59 -0500

    Add query parameter to obtenerCajasAction

--- client/src/redux/cajas/actions/obtenerCajasAction.jsx
+++ client/src/redux/cajas/actions/obtenerCajasAction.jsx
@@ -1,9 +1,9 @@
 import obtenerCajasServices from '../../../services/cajas/obtenerCajasServices.jsx';
 import { cargarCajas } from '../slices/cajasSlices.jsx';
 
-export const obtenerCajasAction = async (dispatch) => {
+export const obtenerCajasAction = async (dispatch, query) => {
 	try {
-		const data = await obtenerCajasServices();
+		const data = await obtenerCajasServices(query);
 
 		dispatch(cargarCajas(data));
 	} catch (error) {
