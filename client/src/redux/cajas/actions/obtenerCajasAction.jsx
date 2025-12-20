c107e38ea8c30cb70b3e0bdc5c97949cb37dec6c
Carlos Castellanos <karlos065@hotmail.com>
2025-12-20 00:06:02 -0500

    Solucion CajaActual

--- client/src/redux/cajas/actions/obtenerCajasAction.jsx
+++ client/src/redux/cajas/actions/obtenerCajasAction.jsx
@@ -1,19 +1,11 @@
 import obtenerCajasServices from '../../../services/cajas/obtenerCajasServices.jsx';
-import { cargarCajas, cargarCajaActual } from '../slices/cajasSlices.jsx';
+import { cargarCajas } from '../slices/cajasSlices.jsx';
 
 export const obtenerCajasAction = async (dispatch, query) => {
 	try {
 		const data = await obtenerCajasServices(query);
 
 		dispatch(cargarCajas(data));
-
-		const cajaActual = data.filter((caja)=> caja.estado === "abierta")
-
-		if(cajaActual.length > 0){
-
-		dispatch(cargarCajaActual(cajaActual[0]))
-
-		}
 	} catch (error) {
 		console.log(error);
 	}
