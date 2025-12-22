46440020cc2cf0145d38db5a630df694b3579753
Carlos Castellanos <karlos065@hotmail.com>
2025-12-22 17:19:07 -0500

    conexion

--- client/src/conexiones/conexiones.jsx
+++ client/src/conexiones/conexiones.jsx
@@ -2,9 +2,9 @@ const server = {
 	api: {
 		// baseURL: 'http://localhost:3000/',
 		// baseURL: 'http://192.168.101.3:3000/',
-		baseURL: 'http://localhost:3000/',
+		// baseURL: 'http://localhost:3000/',
 		// baseURL: 'https://localhost:3000/',
-		// baseURL: 'https://appjuanchitoserver.onrender.com/',
+		baseURL: 'https://appjuanchitoserver.onrender.com/',
 		// baseURL: 'https://8824b071bf3e.ngrok-free.app/',
 	},
 };
